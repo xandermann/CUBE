6a348c620c3acb1f0197e726e14460c6143f08d5
Corentin Vernevaut <corentin.vernevaut@protonmail.com>
2021-03-29 22:33:12 +0200

    migration table complaints / orderable / favorites

---
+++ backend/database/migrations/2021_03_29_191608_create_complaints_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComplaintsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('complaints', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('message');
+            $table->date('date');
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('restaurants_id')->constrained('restaurants');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('complaints');
+    }
+}
---
+++ backend/database/migrations/2021_03_29_194748_create_favorites_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFavoritesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('orderable_id')->constrained('orderable');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favorites');
+    }
+}
