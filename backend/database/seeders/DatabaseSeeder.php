92bec3b573b0508008bd019762a62b3bfe16eebb
Corentin Vernevaut <corentin.vernevaut@protonmail.com>
2021-05-20 22:44:09 +0200

    seeder merge

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -18,22 +18,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // User::factory(100)->create();
+        //User::factory()->count(1)->create();
         
         //population des ingrédients
         Ingredient::factory()->count(20)->create();
-
-        //population des restaurants avec les ingrédients liés (random)
         $ids = range(1, 20);
-        Restaurant::factory()->count(100)->create()->each(function ($restaurant) use($ids) {
-            shuffle($ids);
-            $restaurant->ingredients()->attach(array_slice($ids, 0, rand(1, 3)), ['quantity' => rand(0, 100)]);
-        });
 
         //population des plats avec les ingrédients (random)
-        Dishe::factory()->count(30)->create()->each(function ($dishe) use($ids) {
+        Dishe::factory()->count(20)->create()->each(function ($dishe) use($ids) {
             shuffle($ids);
             $dishe->ingredients()->attach(array_slice($ids, 0, rand(1, 4)), ['quantity' => rand(0, 5)]);
         });
+
+        //population des restaurants avec les ingrédients, plats liés (random)
+        Restaurant::factory()->count(100)->create()->each(function ($restaurant) use($ids) {
+            shuffle($ids);
+            $restaurant->ingredients()->attach(array_slice($ids, 0, rand(1, 3)), ['quantity' => rand(0, 100)]);
+            shuffle($ids); //on remélange pour les plats
+            $restaurant->dishes()->attach(array_slice($ids, 0, rand(1, 3)));
+        });
+
+        
     }
 }
