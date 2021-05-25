f863ba1828e0793b33acf118e2d8a616921887d2
Alexandre HUBLAU <contact@alexandre-hublau.com>
2021-05-25 20:25:56 +0200

    chore(auth): installation of next-sanctum auth

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,65 +1,19 @@
 <template>
-  <div class="container">
-    <div>
-      <h1>AUTHENTIFICATION: voir dans la console :)</h1>
-      <button @click="fetch_token">Fetch token</button>
-      <button @click="login">Login</button>
-      <button @click="fetch_user">Fetch user</button>
-      <button @click="logout">Logout</button>
-    </div>
-  </div>
+  <button @click="login">Connexion</button>
 </template>
 
 <script>
-import axios from 'axios'
-
 export default {
   methods: {
-    fetch_token() {
-      axios
-        .get('http://localhost/sanctum/csrf-cookie', {
-          withCredentials: true,
-        })
-        .then(console.log)
-        .catch(console.error)
-    },
     login() {
-      axios
-        .post(
-          'http://localhost/login',
-          {
-            email: 'a@a.a',
-            password: 'a',
-          },
-          {
-            withCredentials: true,
-          }
-        )
-        .then(console.log)
-        .catch(console.error)
-    },
-    fetch_user() {
-      axios
-        .get('http://localhost/api/user', {
-          withCredentials: true,
-        })
-        .then(console.log)
-        .catch(console.error)
-    },
-    logout() {
-      axios
-        .post(
-          'http://localhost/logout',
-          {},
-          {
-            withCredentials: true,
-          }
-        )
-        .then(console.log)
-        .catch(console.error)
+      console.log(this)
+      this.$auth.loginWith('laravelSanctum', {
+        data: {
+          email: 'a@a.a',
+          password: 'a',
+        },
+      })
     },
   },
 }
 </script>
-
-<style></style>
